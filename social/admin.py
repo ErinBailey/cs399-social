681d449c16a4628f138c16cd89e5f8b1579ef49d
Rick Jamil <shariq1989@gmail.com>
2015-03-31 21:14:05 -0700

    Revert "database commit"
    This reverts commit 2872758d6a17eab58d15eea0be6f739359521d55, reversing
    changes made to 64c64111c4b0cf7bd12dae74daa3b3f9c0ec0c97.

--- social/admin.py
+++ social/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from social.models import UserProfile, Dream, Search
+from social.models import User, Dream, Search
 
 # Register your models here.
 #class SeatsAdmin(admin.ModelSeats):
@@ -7,7 +7,7 @@ from social.models import UserProfile, Dream, Search
 #	admin.site.register(Seats, SeatsAdmin)
 
 
-admin.site.register(UserProfile)
+admin.site.register(User)
 admin.site.register(Dream)
 admin.site.register(Search)
 
